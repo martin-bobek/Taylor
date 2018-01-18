e197fe66278bb588c65cdb82be1179d041120995
martin-bobek <martinbobek77@hotmail.com>
2018-01-18 12:51:14 -0700

    Implemented main

--- Program.java
+++ Program.java
@@ -1,6 +1,7 @@
 public class Program {
 	public static void main(String[] args) {
-		
+		for (int i = 0; i < args.length; i++)
+			System.out.print(toUpper(args[i]));
 	}
 	
 	private static String toUpper(String str) {
