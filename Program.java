2cdb9a82d205213b5dad63b0c3ebcf831e4d1949
martin-bobek <martinbobek77@hotmail.com>
2018-01-18 12:54:32 -0700

    Implemented toUpper

--- Program.java
+++ Program.java
@@ -1,10 +1,16 @@
 public class Program {
 	public static void main(String[] args) {
 		for (int i = 0; i < args.length; i++)
-			System.out.print(toUpper(args[i]));
+			System.out.print(toUpper(args[i]) + ' ');
 	}
 	
 	private static String toUpper(String str) {
-		return new String();
+		String upper = new String();
+		for (int i = 0; i < str.length(); i++)
+			if (str.charAt(i) >= 'a' && str.charAt(i) <= 'z')
+				upper += (char)(str.charAt(i) + 'A' - 'a');
+			else
+				upper += str.charAt(i);
+		return upper;
 	}
 }
